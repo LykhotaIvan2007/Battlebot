44edf90c3b91e51edf91e408f2a6a51fe6cf10ef
LykhotaIvan <ivan.lykhota@student.nhlstenden.com>
2026-03-26 11:59:42 +0100

    added coments

--- Game_Logic/Game_Logic.ino
+++ Game_Logic/Game_Logic.ino
@@ -1,22 +1,27 @@
 #include <Adafruit_NeoPixel.h>
 
-const int SPEED = 160;
-const int RIGHT_MOTOR_SPEED_DERCREMENTATION = 20;
+// SPEED SETTINGS 
+const int SPEED = 160;   // Base speed of motors
+const int SPEED_DIFFERENCE = 20; // Compensation for motor imbalance
+const int SPEED_INCREMENTATION_MOVE_AND_CORRECT = 20; // Speed increase when correcting
+const int SPEED_DECREMENTATION_MOVE_AND_CORRECT = 10; // Speed decrease when correcting
+const int WALL_ALIGNING_DISTANCE = 9; // Distance to maintain from wall
 
-int TURN_90_TIME = 450;
-int TURN_180_TIME = 800;
+int TURN_90_TIME = 600; // Time to turn 90 degrees
 
-const int EMPTY_WAY_DISTANCE = 30;
-const int STOP_DISTANCE = 15;
+const int EMPTY_WAY_DISTANCE = 30; // Distance considered as open path
+const int STOP_DISTANCE = 15; // Distance to stop before obstacle
 
-const int TIME_WHEN_WAY_DETECTED = 250;
-const int TIME_FOR_NEW_PATH = 400;
+const int TIME_WHEN_WAY_DETECTED = 650; // Time delay when new path detected
+const int TIME_FOR_NEW_PATH = 1000; // Time to move into a new path
 
+// MOTOR PINS 
 const int LEFTMOTORBACK = 6;
 const int LEFTMOTORSTRAIGHT = 9;
 const int RIGHTMOTORBACK = 10;
 const int RIGHTMOTORSTRAIGHT = 11;
 
+// ULTRASONIC SENSOR PINS 
 const int ECHOPIN_RIGHT = 2;
 const int TRIGPIN_RIGHT = 3;
 const int ECHOPIN = 4;
@@ -24,10 +29,12 @@ const int TRIGPIN = 5;
 const int ECHOPIN_LEFT = 7;
 const int TRIGPIN_LEFT = 8;
 
+// GRIPPER
 const int SERVO = 12;
-const int GRIPPER_OPEN = 1600;
-const int GRIPPER_CLOSE = 990;
+const int GRIPPER_OPEN = 1600;  // Pulse width for open position
+const int GRIPPER_CLOSE = 990;  // Pulse width for closed position
 
+// LINE SENSOR PINS 
 const int LINE_SENSOR1 = A0;
 const int LINE_SENSOR2 = A1;
 const int LINE_SENSOR3 = A2;
@@ -37,82 +44,108 @@ const int LINE_SENSOR6 = A5;
 const int LINE_SENSOR7 = A6;
 const int LINE_SENSOR8 = A7;
 
+// Array of line sensor pins
 const int sensorPins[8] = {
   LINE_SENSOR1, LINE_SENSOR2, LINE_SENSOR3, LINE_SENSOR4,
   LINE_SENSOR5, LINE_SENSOR6, LINE_SENSOR7, LINE_SENSOR8
 };
 
-int sensorValues[8];
+int sensorValues[8]; // Stores values from sensors
 
-const int NUM_SENSORS = 8;
+const int NUM_SENSORS = 8; // Total number of line sensors
 
-int blackDetection = 900;
+int blackDetection = 900; // Threshold value to detect black line
 
+// DISTANCE VARIABLES 
 long distanceFront, distanceRight, distanceLeft;
 
+// LED bulbs
 Adafruit_NeoPixel pixels(4, 13, NEO_GRB + NEO_KHZ800);
 
 void setup(){
-  Serial.begin(9600);
+  // Set motor pins as output
   pinMode(LEFTMOTORBACK, OUTPUT);
   pinMode(LEFTMOTORSTRAIGHT, OUTPUT);
   pinMode(RIGHTMOTORBACK, OUTPUT);
   pinMode(RIGHTMOTORSTRAIGHT, OUTPUT);
+
+  // Set ultrasonic sensor pins
   pinMode(TRIGPIN_RIGHT, OUTPUT);
   pinMode(ECHOPIN_RIGHT, INPUT);
   pinMode(TRIGPIN, OUTPUT);
   pinMode(ECHOPIN, INPUT);
   pinMode(TRIGPIN_LEFT, OUTPUT);
   pinMode(ECHOPIN_LEFT, INPUT);
+
+  // Gripper setup
   pinMode(SERVO, OUTPUT);
   holdGripper(GRIPPER_OPEN, 1000);
+  
+  // Line sensor pins setup
   for (int i = 14; i < 22; i++){
     pinMode(i, INPUT);
   }
+
+  // Initialize LED bulbs
   pixels.begin();      
   pixels.clear();      
   pixels.show();       
 }
 
-boolean parkingMode = true;
-boolean mode = true; // true = maze || false = line
+// MODES 
+boolean parkingMode = true; // Initial parking routine active
+boolean mode = true; // true = maze mode, false = line following mode
 
 void loop(){
-  gripper(0);
+  gripper(0); // Maintain gripper signal
+
+  // Read distances from sensors
   distanceLeft = readDistance(TRIGPIN_LEFT, ECHOPIN_LEFT);
   distanceRight = readDistance(TRIGPIN_RIGHT, ECHOPIN_RIGHT);
   distanceFront = readDistance(TRIGPIN, ECHOPIN);
-  if(distanceFront < 30 && parkingMode == true){
-    //delay(1000);
-    while (distanceLeft < 10) {
-      moveForward(SPEED, SPEED);
-      distanceLeft = readDistance(TRIGPIN_LEFT, ECHOPIN_LEFT);
-    }
-    delay (750);
-    stopMoving();
-    holdGripper(GRIPPER_CLOSE, 1000);
-    turnLeft();
-    turnLeft();
-    moveForward(SPEED, SPEED);
-    delay(700);
-    moveAndCorrect(3000);
-    parkingMode = false;
-  }
+
+  start(); // Execute starting sequence
+
   if (!parkingMode){
     if (checkLine()){
-      lineFollowing();
+      lineFollowing(); // Follow line if detected
     }else{
-      maze();
+      maze(); // Otherwise use maze navigation
     }
   }
   
-  //pixels.setPixelColor(0, pixels.Color(255, 0, 0));   // зеленый 0 - левый нижний
-  //pixels.setPixelColor(1, pixels.Color(255, 255, 0)); // жёлтый  1 - правый нижний 
-  //pixels.setPixelColor(2, pixels.Color(0, 0, 255));   // синий 2 - правый верхний
-  //pixels.setPixelColor(3, pixels.Color(0, 255, 0));  // крассный 3 - левый верхний
-  //pixels.show();
 }
 
+// START LOGIC 
+void start(){
+  // If cone detected in front during parking mode
+  if(distanceFront < 30 && parkingMode == true){
+    delay(3000);
+
+    // Move forward until left side is clear
+    while (distanceLeft < 10){
+      moveForward(SPEED, SPEED);
+      distanceLeft = readDistance(TRIGPIN_LEFT, ECHOPIN_LEFT);
+    }
+
+    delay (750);
+    stopMoving();
+
+    holdGripper(GRIPPER_CLOSE, 1000); // Close gripper (grab object)
+
+    //turnLeft();
+    turnLeft(); // Turn around
+
+    moveForward(SPEED, SPEED);
+    delay(700);
+
+    moveAndCorrect(3000); // Move forward with wall correction
+
+    parkingMode = false; // Exit parking mode
+  }
+}
+
+// GRIPPER CONTROL 
 void holdGripper(int pulse,int timeMs){
   unsigned long start = millis();
   while(millis() - start < timeMs){
@@ -124,54 +157,69 @@ void holdGripper(int pulse,int timeMs){
 void gripper(int newPulse){
   static unsigned long timer;
   static int pulse;
+
   if(millis() > timer){
     if(newPulse > 0){  
       pulse = newPulse;  
     }  
+
     digitalWrite(SERVO, HIGH);  
     delayMicroseconds(pulse);  
     digitalWrite(SERVO, LOW);  
-    timer = millis() + 20;
+
+    timer = millis() + 20; // Repeat every 20ms
   }
 }
 
+// MOVE WITH WALL CORRECTION
 void moveAndCorrect(int duration){
   unsigned long startTime = millis();
+
   while (millis() - startTime < duration){
     long currentLeft = readDistance(TRIGPIN_LEFT, ECHOPIN_LEFT);
     long currentRight = readDistance(TRIGPIN_RIGHT, ECHOPIN_RIGHT);
+
+    // Stop if obstacle detected ahead
     if (readDistance(TRIGPIN, ECHOPIN) < STOP_DISTANCE){
       stopMoving();
       break;
     }
-    if (currentLeft < 9){
-      moveForward(SPEED + 30, SPEED - 10);
+
+    // Adjust direction based on wall closeness
+    if (currentLeft < WALL_ALIGNING_DISTANCE){
+      moveForward(SPEED + SPEED_INCREMENTATION_MOVE_AND_CORRECT, SPEED - SPEED_DECREMENTATION_MOVE_AND_CORRECT);
     }
-    else if (currentRight < 9){
-      moveForward(SPEED - 10, SPEED + 30);
+    else if (currentRight < WALL_ALIGNING_DISTANCE){
+      moveForward(SPEED - SPEED_DECREMENTATION_MOVE_AND_CORRECT, SPEED + SPEED_INCREMENTATION_MOVE_AND_CORRECT);
     }
     else{
       moveForward(SPEED, SPEED);
     }
+
     delay(20);
   }
+
   stopMoving();
 }
 
+// BASIC MOVEMENT 
 void moveForward(int leftSpeed, int rightSpeed){
   pixels.setPixelColor(0, pixels.Color(0, 0, 0));   
   pixels.setPixelColor(1, pixels.Color(0, 0, 0)); 
   pixels.setPixelColor(2, pixels.Color(0, 0, 255));   
   pixels.setPixelColor(3, pixels.Color(0, 0, 255));
   pixels.show();
+
   leftSpeed = constrain(leftSpeed, 0, 255);
   rightSpeed = constrain(rightSpeed, 0, 255);
+
   analogWrite(LEFTMOTORSTRAIGHT, leftSpeed);
   analogWrite(LEFTMOTORBACK, 0);
   analogWrite(RIGHTMOTORSTRAIGHT, rightSpeed);
   analogWrite(RIGHTMOTORBACK, 0);
 }
 
+// Stop all motors
 void stopMoving(){
   analogWrite(RIGHTMOTORSTRAIGHT, 0);
   analogWrite(LEFTMOTORSTRAIGHT, 0);
@@ -179,81 +227,97 @@ void stopMoving(){
   analogWrite(LEFTMOTORBACK, 0);
 }
 
+// TURNING 
 void turnLeft(){
   pixels.setPixelColor(0, pixels.Color(255, 255, 0));
   pixels.setPixelColor(1, pixels.Color(0, 0, 0));
   pixels.setPixelColor(2, pixels.Color(0, 0, 0));
   pixels.setPixelColor(3, pixels.Color(255, 255, 0)); 
   pixels.show();
+
   analogWrite(LEFTMOTORSTRAIGHT, 0);
-  analogWrite(LEFTMOTORBACK, SPEED + 20);
+  analogWrite(LEFTMOTORBACK, SPEED + SPEED_DIFFERENCE);
   analogWrite(RIGHTMOTORSTRAIGHT, SPEED);
   analogWrite(RIGHTMOTORBACK, 0);
+
   delay(TURN_90_TIME);
   stopMoving();
 }
 
 void turnRight(){
-  pixels.setPixelColor(0, pixels.Color(0, 0, 0));   // зеленый 0 - левый нижний
-  pixels.setPixelColor(1, pixels.Color(255, 255, 0)); // жёлтый  1 - правый нижний 
-  pixels.setPixelColor(2, pixels.Color(255, 255, 0));   // синий 2 - правый верхний
-  pixels.setPixelColor(3, pixels.Color(0, 0, 0));  // крассный 3 - левый верхний
+  pixels.setPixelColor(0, pixels.Color(0, 0, 0));   
+  pixels.setPixelColor(1, pixels.Color(255, 255, 0)); 
+  pixels.setPixelColor(2, pixels.Color(255, 255, 0));   
+  pixels.setPixelColor(3, pixels.Color(0, 0, 0));  
   pixels.show();
+
   analogWrite(LEFTMOTORSTRAIGHT, SPEED);
   analogWrite(LEFTMOTORBACK, 0);
   analogWrite(RIGHTMOTORSTRAIGHT, 0);
   analogWrite(RIGHTMOTORBACK, SPEED + 10);
-  delay(600);
+
+  delay(TURN_90_TIME);
   stopMoving();
 }
 
 void turnAround(){
   moveBack();
   delay(500);
-  analogWrite(LEFTMOTORSTRAIGHT, SPEED+20);
+
+  analogWrite(LEFTMOTORSTRAIGHT, SPEED + SPEED_DIFFERENCE);
   analogWrite(LEFTMOTORBACK, 0);
   analogWrite(RIGHTMOTORSTRAIGHT, 0);
-  analogWrite(RIGHTMOTORBACK, SPEED+20);
+  analogWrite(RIGHTMOTORBACK, SPEED + SPEED_DIFFERENCE);
+
   delay(700);
   stopMoving();
 }
 
+// Move backward slightly
 void moveBack(){
   pixels.setPixelColor(0, pixels.Color(0, 255, 0));   
   pixels.setPixelColor(1, pixels.Color(0, 255, 0)); 
   pixels.setPixelColor(2, pixels.Color(0, 0, 0));   
   pixels.setPixelColor(3, pixels.Color(0, 0, 0));  
   pixels.show();
+
   analogWrite(LEFTMOTORSTRAIGHT, 0);
   analogWrite(LEFTMOTORBACK, SPEED);
   analogWrite(RIGHTMOTORSTRAIGHT, 0);
-  analogWrite(RIGHTMOTORBACK, SPEED - RIGHT_MOTOR_SPEED_DERCREMENTATION);
+  analogWrite(RIGHTMOTORBACK, SPEED - SPEED_DIFFERENCE);
+
   delay(350);
   stopMoving();
 }
 
+// ULTRASONIC SENSOR 
 long readDistance(int trig, int echo){
   digitalWrite(trig, LOW);
   delayMicroseconds(2);
+
   digitalWrite(trig, HIGH);
   delayMicroseconds(10);
   digitalWrite(trig, LOW);
+
   long duration = pulseIn(echo, HIGH, 6000);
+
   if (duration == 0){
-    return 1000;
+    return 1000; // No signal = far distance
   }
-  return duration * 0.0343 / 2;
+
+  return duration * 0.0343 / 2; // Convert to cm
 }
 
+// MAZE NAVIGATION 
 void maze(){
   stopMoving();
 
-  if (distanceFront < 9){
+  if (distanceFront < WALL_ALIGNING_DISTANCE){
     moveBack();
     return;
   }
 
-  if (distanceLeft > EMPTY_WAY_DISTANCE){
+  if (distanceLeft > EMPTY_WAY_DISTANCE){  // left hand rule
     moveAndCorrect(TIME_WHEN_WAY_DETECTED);
     stopMoving();
 
@@ -275,17 +339,20 @@ void maze(){
   }
 }
 
-void readSensors() {
-  for (int i = 0; i < NUM_SENSORS; i++) {
-    sensorValues[i] = analogRead(sensorPins[i]);
+// LINE FOLLOWING 
+void readSensors() { // reads all sensors value
+  for (int i = 0; i < NUM_SENSORS; i++) {  
+    sensorValues[i] = analogRead(sensorPins[i]); 
   }
 }
 
+// checks if line detected
 bool checkLine(){
   readSensors();
+
   for (int i = 0; i < NUM_SENSORS; i++){
     if  (sensorValues[i] >= blackDetection){
-      return true;
+      return true; // Line detected
     }
   }
   return false;
@@ -294,7 +361,10 @@ bool checkLine(){
 void lineFollowing()
 {
   readSensors();
+
   bool allBlack = true;
+
+  // Check if all sensors detect black (finish condition)
   for (int i = 0; i < NUM_SENSORS; i++) {
     if (sensorValues[i] < blackDetection) {
       allBlack = false;
@@ -304,16 +374,20 @@ void lineFollowing()
 
   if (allBlack){
     stopMoving();
+
     pixels.setPixelColor(0, pixels.Color(255, 0, 0));   
     pixels.setPixelColor(1, pixels.Color(255, 0, 0));  
     pixels.setPixelColor(2, pixels.Color(255, 0, 0));   
     pixels.setPixelColor(3, pixels.Color(255, 0, 0));  
     pixels.show();
+
     holdGripper(GRIPPER_OPEN, 2000); 
+
     while (true){
-      stopMoving();
+      stopMoving(); // Stay stopped forever
     }
   }
+  // Adjust movement based on sensor value
   else if (sensorValues[3] >= blackDetection && sensorValues[4] >= blackDetection){
     moveForward(180, 180);
   }else if (sensorValues[5] >= blackDetection){
